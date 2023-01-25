b9d52f9ad8c15c9a446116c903a84e3d8b117bf9
Huseyin-Gunaydin <gunaydinhsueyin1453@gmail.com>
2023-01-25 14:27:28 +0100

    java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("Hüseyin Günaydin");
-        System.out.println("Java ");
+        System.out.println("Java dsdsd");
     }
 }
